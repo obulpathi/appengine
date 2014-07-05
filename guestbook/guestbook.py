e5140d3ccf6547e1a0e148dd01086a40ab40424b
Obulapathi N Challa <obulpathi@gmail.com>
2014-07-04 20:06:34 -0400

    guestbook

---
+++ guestbook/guestbook.py
@@ -0,0 +1,22 @@
+import cgi
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/sign', Guestbook)], debug=True)
